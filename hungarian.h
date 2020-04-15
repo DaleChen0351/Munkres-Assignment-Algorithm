d18ae0c38a02c462b45fd87474cb5b392bf776cb
Dale Chen <dalechen0351@google.com>
2020-04-15 17:01:56 +0800

    added some private methods

--- hungarian.h
+++ hungarian.h
@@ -23,11 +23,25 @@ class HungarianMatch
 {
 public:
     HungarianMatch(const int rows, const int cols);
-    void process(float* inMatrix,float* outMatch,int& rows, int& cols);
+    void Process(float* inMatrix,float* outMatch,int& rows, int& cols);
 
 private:
     int nRows;
     int nCols;
+    int step;
+    
+    //
+    void step_one();
+    void step_two();
+    void step_three();
+    void step_four();
+    void step_five();
+    void step_six();
+    //show hungarian info
+    void show_costMatrix();
+    void show_maskMatrix();
+    void show_rowCover();
+    void show_colCover();
     
 
 
